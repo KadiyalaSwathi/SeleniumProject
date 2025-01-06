47e7e620a7611c31d0740bc34127f112dbf7a365
kadiyalaswathi <kadiyalaswathi5@gmail.com>
2025-01-06 23:05:04 +0530

    optimized calculator program

--- src/assignments/Calculator.java
+++ src/assignments/Calculator.java
@@ -3,54 +3,51 @@ import java.util.Scanner;
 
 public class Calculator {
 
+    public static Scanner scanner;
+
     public static void main(String[] args) {
 
+        scanner = new Scanner(System.in);
+        System.out.print("Enter x value: ");
+        int x= scanner.nextInt();
+        System.out.print("Enter y value: ");
+        int y= scanner.nextInt();
         Calculator cl = new Calculator();
         cl.addition();
-        int diff = cl.subtraction();
-        System.out.println("Diff b/n a and b is: "+diff);
+        int diff = cl.subtraction(x,y);
+        System.out.println("Diff b/n x and y is: "+diff);
         multiplication();
-        cl.div(100,20);
+        cl.div(x,y);
 
     }
 
     public void addition(){
-
-        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a value: ");
-        int a = Integer.parseInt(scanner.next());
+        int a = scanner.nextInt();
         System.out.print("Enter b value: ");
-        int b = Integer.parseInt(scanner.next());
+        int b = scanner.nextInt();
         int c=a+b;
         System.out.println("addition of a and b values are: "+c);
 
     }
 
-    public int subtraction(){
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a value: ");
-        int a = Integer.parseInt(scanner.next());
-        System.out.print("Enter b value: ");
-        int b = Integer.parseInt(scanner.next());
+    public int subtraction(int a, int b){
         return a-b;
 
     }
 
     public static void multiplication(){
-
-        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a value: ");
-        int a = Integer.parseInt(scanner.next());
+        int a = scanner.nextInt();
         System.out.print("Enter b value: ");
-        int b = Integer.parseInt(scanner.next());
+        int b = scanner.nextInt();
         int c=a*b;
         System.out.println("Multiplication of a and b is:"+c);
     }
 
-    public void div(int a,int b){
-        int c=a/b;
-        System.out.println("Divison of a and b is:"+c);
+    public void div(int x,int y){
+        int z=x/y;
+        System.out.println("Divison of x and y is:"+z);
     }
 
 }
