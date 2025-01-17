8ab0f6363a0c0fd6bb0d8972735fee74beef441c
kadiyalaswathi <kadiyalaswathi5@gmail.com>
2025-01-17 20:52:47 +0530

    Committing code related to final, finally, finalize, throw and throws

---
+++ src/exceptionHandling/ExampleFinalize.java
@@ -0,0 +1,14 @@
+package exceptionHandling;
+
+public class ExampleFinalize {
+
+    public static void main(String[] args) {
+
+        System.out.println("Test program");
+        ExampleFinalize ef = new ExampleFinalize();
+        System.gc();
+    }
+
+
+
+}
